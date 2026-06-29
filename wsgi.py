9d3de2222dcddb628140fc3f748d530c74f2f816
bilirodriguestrebejo-dotcom <bilirodriguestrebejo@gmail.com>
2026-06-29 17:58:46 -0600

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,6 @@ from pathlib import Path
 
 PROJECT_DIR = Path(__file__).resolve().parent / "chatbot 5"
 sys.path.insert(0, str(PROJECT_DIR))
+sys.path.insert(0, str(PROJECT_DIR / "backend"))
 
-from backend.appy import app
+from appy import app
